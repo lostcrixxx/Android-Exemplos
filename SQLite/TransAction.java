36954b2ac95fd90477f2241a984b52c7e3065a2c
Cristiano morais <devcristianomorais@gmail.com>
2019-10-25 14:17:11 -0300

    Exemplo de insert com transAction

---
+++ SQLite/TransAction.java
@@ -0,0 +1,33 @@
+  public class exampleTransAction {
+	 private SQLiteOpenHelper dbHelper = null;
+	  
+    public void insertValueOnTableNTEvent(String tableName, ArrayList<Object> insp) {
+//        int count = 1;
+//        android.util.Log.e("DatabaseManager", "Teste insert NTEvent INICIO");
+
+        // you can use INSERT only
+        String sql = "INSERT OR REPLACE INTO " + tableName + " ( id, name, descricao ) VALUES ( ?,?,? )";
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        db.beginTransactionNonExclusive();
+        // db.beginTransaction();
+
+        SQLiteStatement stmt = db.compileStatement(sql);
+
+        for (int i = 0; i <= insp.size(); i++) {
+            stmt.bindLong(1, insp.get(i).getId());
+            stmt.bindDouble(2, insp.get(i).getName());
+            stmt.bindString(3, insp.get(i).getDescription());
+
+            stmt.execute();
+            stmt.clearBindings();
+//            android.util.Log.e("DatabaseManager", "Teste insert NTEvent PROCESSANDO " + count++);
+        }
+
+        db.setTransactionSuccessful();
+        db.endTransaction();
+
+        db.close();
+    }
+}
