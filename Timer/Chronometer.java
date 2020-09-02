1725f3fad377ef998c06c05c5e93f11b2f66bfbd
Cristiano morais <cristiano.morais@brastan.com>
2020-09-02 20:14:23 -0300

    added timer

---
+++ Timer/Chronometer.java
@@ -0,0 +1,8 @@
+// keep track of execution time
+long lStartTime = System.nanoTime();
+
+// execution finised
+long lEndTime = System.nanoTime();
+
+// display execution time
+long timeElapsed = lEndTime - lStartTime;
