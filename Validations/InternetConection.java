4d5bdc666a4f74c86d9749930d61d856ce7bf8c1
Cristiano M <devcristianomorais@gmail.com>
2021-09-25 22:33:52 -0300

    update internet connection

--- Validations/InternetConection.java
+++ Validations/InternetConection.java
@@ -1,19 +1,34 @@
-public static boolean isConnected(Context cont){
- ConnectivityManager conmag = (ConnectivityManager)cont.getSystemService(Context.CONNECTIVITY_SERVICE);
-
- if ( conmag != null ) {
-  conmag.getActiveNetworkInfo();
-
-  //Verifica internet pela WIFI
-  if (conmag.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected()) {
-   return true;
-  }
-
-  //Verifica se tem internet móvel
-  if (conmag.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected()) {
-   return true;
-  }
- }
-
- return false;
+@IntRange(from = 0, to = 3)
+public static int isConnected(Context context) {
+    int result = 0; // Returns connection type. 0: none; 1: mobile data; 2: wifi
+    ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (cm != null) {
+            NetworkCapabilities capabilities = cm.getNetworkCapabilities(cm.getActiveNetwork());
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    result = 2;
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    result = 1;
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
+                    result = 3;
+                }
+            }
+        }
+    } else {
+        if (cm != null) {
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            if (activeNetwork != null) {
+                // connected to the internet
+                if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                    result = 2;
+                } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                    result = 1;
+                } else if (activeNetwork.getType() == ConnectivityManager.TYPE_VPN) {
+                    result = 3;
+                }
+            }
+        }
+    }
+    return result;
 }
